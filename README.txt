8e85bac3de815f469cefa30c26e1f4ebe9aa789e
sam hotiky <178449962@qq.com>
2019-03-01 10:21:31 +0800

    make FatFS run in MinGW with the portion implemented in RAMdisk

---
+++ README.txt
@@ -0,0 +1,1 @@
+make FatFS run in MinGW
---
+++ fatfs/source/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+#include "ff.h"
+
+FATFS FatFs;   /* Work area (filesystem object) for logical drive */
+
+int main (void)
+{
+    FIL fil;
+    char buffer[100];
+    UINT br, bw;
+    FRESULT res;
+    BYTE work[FF_MAX_SS];
+
+    if (res = f_mkfs("0:", FM_ANY, 0, work, sizeof(work))) {
+        printf("mkfs error! res=%d\n", res);
+        return 1;
+    }
+    printf("mkfs successfully\n");
+
+    if (res = f_mount(&FatFs, "0:", 1)) {
+        printf("mount error! res=%d\n", res);
+        return 1;
+    }
+    printf("mount successfully\n");
+
+    if (f_open(&fil, "message.txt", FA_WRITE | FA_CREATE_ALWAYS)) {
+        printf("open error! res=%d\n", res);
+        return 1;
+    }
+    printf("open successfully\n");
+
+    if (f_write(&fil, "hello fatfs", strlen("hello fatfs"), &bw)) {
+        printf("write error! res=%d\n", res);
+        return 1;
+    }
+    printf("write successfully, wrote: %s\n", "hello fatfs");
+
+    f_close(&fil);
+
+    if (f_open(&fil, "message.txt", FA_READ)) {
+        printf("open error! res=%d\n", res);
+        return 1;
+    }
+    printf("open successfully\n");
+
+    if (f_read(&fil, buffer, sizeof(buffer), &br)) {
+        printf("read error! res=%d\n", res);
+        return 1;
+    }
+    printf("read successfully, read: %s\n", buffer);
+
+    f_close(&fil);
+
+    return 0;
+}
+
